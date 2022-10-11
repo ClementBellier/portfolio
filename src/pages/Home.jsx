c142ebc793fe0b2ebdea9ec69b2877e7430c1bb8
Clement BELLIER <clement.bellier@gmail.com>
2022-10-11 15:48:02 +0200

    Flèche pour indiquer le scroll au début

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,6 +8,9 @@ function Home() {
         <h1>{HOME.WELCOME}</h1>
         <h2>{`${HOME.I_AM} ${HOME.FULLNAME}`}</h2>
         <h3>{HOME.TITLE}</h3>
+        <svg className="home-scroll-down">
+          <use href="#chevron-down" />
+        </svg>
       </section>
       <AboutMe />
     </>
